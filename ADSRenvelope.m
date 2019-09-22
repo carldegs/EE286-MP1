85c8791f3e361751535fba9eab4812acc1491010
narzdavid <narzdavid@gmail.com>
2019-09-23 04:21:06 +0800

    Changed linear attack to logarithmic rise

--- ADSRenvelope.m
+++ ADSRenvelope.m
@@ -1,5 +1,4 @@
 function [t,y] = ADSRenvelope(a,d,s,r,pressed,fs)
-    attack = 0:1/fs/a:1; % linear increase to 100%
     attack=1-exp(log(0.001)/a*(0:1/fs/a:1));
     decay = exp(log(s)/d*(0:1/fs:d));  % exponential decay
     sustain = ones(1, round((pressed-a-d)*fs))*s; % sustain  
