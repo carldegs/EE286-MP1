c41a7e798ee01f62cacc03fd4e3f7d8e0c228ea1
narzdavid <narzdavid@gmail.com>
2019-09-23 04:17:37 +0800

    Add envelope plot image

--- adsr_test.m
+++ adsr_test.m
@@ -12,7 +12,8 @@ figure(1);plot(tenv, env);
 %% Test 2 Test ADSR Extraction, piano
 [x,fs]=audioread('Piano_mf_A4.wav');
 [a,d,s,r,tP,P,sDuration] = getADSR(x,fs);
-figure(2); subplot(3,1,1);
+figure(2); 
+subplot(3,1,1);
 offset = max(tP)-(a+d+sDuration+r);
 plot(tP, P);hold on; 
 plot(offset, P(find(tP>=offset,1)), 'go'); 
@@ -20,9 +21,9 @@ plot(a+offset, P(find(tP>=a+offset,1)), 'rx');
 plot(a+d+offset, P(find(tP>a+d+offset,1)), 'rx'); 
 plot(linspace(offset+a+d, a+d+sDuration+offset ,100), s*ones(100), '--'); 
 plot(offset+a+d+sDuration, P(find(tP>=a+d+sDuration+offset,1)), 'rx'); 
-
 [tenv, env] = ADSRenvelope(a,d,s,r,sDuration+a+d,16000);
 plot(tenv+offset,env, 'r'); hold off;
+title('Piano ADSR Envelope');
 
 %% Test 3 Test ADSR Extraction, snare
 [x,fs]=audioread('snare.wav');
@@ -35,6 +36,7 @@ plot(linspace(a+d, a+d+sDuration ,100), s*ones(100), '--');
 plot(a+d+sDuration, P(find(tP==a+d+sDuration)), 'rx'); 
 
 [tenv, env] = ADSRenvelope(a,d,s,r,sDuration+a+d,16000);
+title('Snare ADSR Envelope');
 plot(tenv,env, 'r'); hold off;
 
 
@@ -50,6 +52,7 @@ plot(a+d+sDuration, P(find(tP==a+d+sDuration)), 'rx');
 
 [tenv, env] = ADSRenvelope(a,d,s,r,sDuration+a+d,16000);
 plot(tenv,env, 'r'); hold off;
+title('Trumpet ADSR Envelope');
 
 
 
