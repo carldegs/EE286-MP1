9248ed64f5ae8fd0cbeb5286cb9444cda9961763
Carl de Guia <carl.2795@gmail.com>
2019-09-29 10:13:17 +0800

    fix issues

--- plot_spectrogram.m
+++ plot_spectrogram.m
@@ -5,7 +5,7 @@ function [S, F, T] = plot_spectrogram( file_path, max_freq )
     % soundsc(x,fs);
     figure();
     [S, F, T] = spectrogram(x, 256*4, [], 0:max_freq, fs, 'yaxis');
-    spectrogram(x, 256*4, [], 0:max_freq, fs, 'yaxis');
+    spectrogram(x, power(2,10), [], 0:max_freq, fs, 'yaxis');
     title(file_path);
 end
 
--- synthesis.m
+++ synthesis.m
@@ -1,6 +1,6 @@
 %% PLOT SPECTOGRAM OF THE INSTRUMENTS
 close all;
-clear all;
+clear;
 
 plot_spectrogram('Piano_mf_A4.wav', 5000);
 plot_spectrogram('snare.wav', 10000);
@@ -8,6 +8,8 @@ plot_spectrogram('Trumpet_novib_A4.wav', 15000);
 
 %% SYNTHESIZE PIANO
 close all;
+clear;
+
 plot_spectrogram('Piano_mf_A4.wav', 5000);
 % Based on the spectrogram, the pitch/fundamental freq
 % is at 440Hz
@@ -15,7 +17,6 @@ plot_spectrogram('Piano_mf_A4.wav', 5000);
 [x, fs] = audioread('Piano_mf_A4.wav');
 t = 0 : 1/fs : length(x)/fs;
 ff = 440;   % fundamental frequency
-amp = [1, 0.8, 0.6, 0.4, 0.2, 0.1];
 
 % create the envelope
 env = hilbert(x, length(t));
@@ -27,12 +28,6 @@ env = env';
 P = interp1(adsr_time, P, t);
 P = P ./ max(P);
 
-figure();
-plot(t,P);
-
-figure();
-plot(t, env);
-figure();
 signal = zeros(length(x) + 1,1)';
 
 for k = 1:6
@@ -45,15 +40,14 @@ for k = 1:6
     signal = signal + new_signal;
 end
 
+% add filters to remove unnecessary frequencies
+[b,a] = butter(2, 100 / fs, 'high');
+signal = filter(b,a,signal);
+
 signal = signal ./ max(abs(signal));
 
-% add a high pass filter 
-% [b,a] = butter(6, 4000 / fs, 'low');
-% signal = filter(b,a,signal);
-% [b,a] = butter(6, 100 / fs, 'high');
-% signal = filter(b,a,signal);
-
-spectrogram(signal, 256*4, [], 0:5000, fs, 'yaxis');
+figure();
+spectrogram(signal, power(2,10), [], 0:5000, fs, 'yaxis');
 
 soundsc(x, fs);
 pause(5);
